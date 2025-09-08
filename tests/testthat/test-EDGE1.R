46d82b8092567cf3436530a76992b25348048592
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2025-09-08 17:59:38 +0200

    add tests

---
+++ tests/testthat/test-EDGE1.R
@@ -0,0 +1,19 @@
+
+
+rl.data <- data.frame(
+  "species" = c("Draco_drogon", "Draco_viseryi", "Draco_rheagalii"),
+  "RL.cat"   = c("NT", "CR", "EW")
+  )
+
+tree <- ape::read.tree()
+
+
+
+
+
+test_that("EDGE1 runs", {
+
+  skip_on_ci()
+  skip_on_cran()
+
+})
