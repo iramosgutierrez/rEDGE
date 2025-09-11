6ae83b3059544ba941c39d5eeb7e9678c865bf7b
iramosgutierrez <ignacio.ramosgutierrez@uam.es>
2025-09-11 16:52:17 +0200

    add some tests

--- tests/testthat/test-EDGE1.R
+++ tests/testthat/test-EDGE1.R
@@ -2,18 +2,22 @@
 
 rl.data <- data.frame(
   "species" = c("Draco_drogon", "Draco_viseryi", "Draco_rheagalii"),
-  "RL.cat"   = c("NT", "CR", "EW")
+  "RL.cat"   = c("NT", "CR", "LC")
   )
 
-tree <- ape::read.tree()
+tree <- ape::read.tree(text = "((Draco_rheagalii:2.5,Draco_viseryi:2.5):1,Draco_drogon:3.5);")
 
-
-
+edge <- calculate_EDGE1(tree, rl.data)
 
 
 test_that("EDGE1 runs", {
 
   skip_on_ci()
   skip_on_cran()
+
+  expect(class(edge) == "data.frame", failure_message = "outupt should be a data frame")
+  expect_equal(edge$ED, c(3.5, 3, 3))
+  expect_equal(round(edge$EDGE, digits = 6) , c(2.197225, 1.386294, 4.158883),
+         failure_message = "Unexpected EDGE1 values")
 
 })
--- tests/testthat/test-EDGE2.R
+++ tests/testthat/test-EDGE2.R
@@ -1,15 +1,20 @@
 
-data <- data.frame(Names = c("Pippin", "Frodo"),
-                   Affil = c("Home", NA))
-path <- tempdir()
 
-test_that("PDF file is created in the provided path", {
+rl.data <- data.frame(
+  "species" = c("Draco_drogon", "Draco_viseryi", "Draco_rheagalii"),
+  "RL.cat"   = c("NT", "CR", "LC")
+)
 
-  skip_on_cran()
-  skip_on_ci()
+tree <- ape::read.tree(text = "((Draco_rheagalii:2.5,Draco_viseryi:2.5):1,Draco_drogon:3.5);")
 
-  create_badge(data, path = path, name.column = "Names", affiliation.column = "Affil")
-  expect_true(file.exists(file.path(path, "Badges.pdf")))
+edge_df <- calculate_EDGE2(tree, rl.data, sort.list = T)
+edge_ls <- calculate_EDGE2(tree, rl.data,return.all = T)
+
+test_that("Testing EDGE2", {
+
+expect(inherits(edge_df, "data.frame"), "Edge_df should be a data frame")
+expect(inherits(edge_ls, "list"), "Edge_ls should be a list")
+
 
 })
 
