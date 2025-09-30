eac91b30a3b3e0e72a71153cfd777a0ef69470cf
Ignacio <ignacio.ramosgutierrez@uam.es>
2025-09-30 10:05:13 +0200

    document data

---
+++ R/data.R
@@ -0,0 +1,23 @@
+
+#' Phylogeny of monotremes
+#'
+#' Example phylogeny for EDGEcalc package
+#'
+#' @format A phylogenetic tree with 5 tips, including platypus and echidnas.
+#' \describe{
+#' Phylogenetic tree with 5 species of monotremes (Monotremata).
+#' This is a subtree extracted from the complete mammal phylogeny by Upham et al. (2019)
+#' }
+"monotreme.tree"
+
+
+#' monotreme.table
+#'
+#' Information data frame with Red List assessments for the species in the monotreme phylogeny.
+#'
+#' @format An info data frame
+#' \describe{
+#' Data frame with Red List assessments for the 5 species of monotremes (IUCN, September 2025)
+#'
+#' }
+"monotreme.table"
