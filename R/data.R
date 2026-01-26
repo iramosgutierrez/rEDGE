e81703911a74c1bdd73f2f660d4b644d0f478c3e
Ignacio <ignacio.ramosgutierrez@uam.es>
2026-01-26 21:45:42 +0100

    PDi advances

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 
 #' Phylogeny of monotremes
 #'
-#' Example phylogeny for EDGEcalc package
+#' Example phylogeny for rEDGE package
 #'
 #' @format A phylogenetic tree with 5 tips, including platypus and echidnas.
 #' \describe{
@@ -15,9 +15,32 @@
 #'
 #' Information data frame with Red List assessments for the species in the monotreme phylogeny.
 #'
-#' @format An info data frame
+#' @format An data frame with RL assessments
 #' \describe{
 #' Data frame with Red List assessments for the 5 species of monotremes (IUCN, September 2025)
 #'
 #' }
 "monotreme.table"
+
+
+#' crocodile.trees
+#'
+#' Example phylogeny for rEDGE package
+#'
+#' @format A multiPhylo object storing 10 crocodile phylogenies
+#' \describe{
+#' multiPhylo object with 10 phylogenies including 27 species of crocodiles.
+#' }
+"crocodile.trees"
+
+
+#' crocodile.table
+#'
+#' Information data frame with Red List assessments for the species in the crocodile phylogeny for years 2023 and 2024.
+#'
+#' @format An info data frame
+#' \describe{
+#' Data frame with Red List assessments for the 27 species of monotremes for years 2023 & 2024.
+#'
+#' }
+"crocodile.table"
